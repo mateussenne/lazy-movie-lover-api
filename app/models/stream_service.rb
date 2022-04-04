9fed76dfc19d5c4018f51f5b4be5422da52bf317
Mateus Senne <mateussenne@gmail.com>
2022-04-04 18:06:01 -0300

    Add code creation in before save hook

--- app/models/stream_service.rb
+++ app/models/stream_service.rb
@@ -3,4 +3,11 @@
 class StreamService < ApplicationRecord
   has_many :movies, dependent: :destroy
   validates :name, :base_url, presence: true
+  before_create :set_service_code
+
+  private
+
+  def set_service_code
+    self.code = name.parameterize
+  end
 end
