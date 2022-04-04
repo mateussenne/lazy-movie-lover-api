f6c8d7e577efbb4af3958bcc3bb5887ba7f1a648
Mateus Senne <mateussenne@gmail.com>
2022-04-04 18:00:39 -0300

    Update schema 04/04 18:00

---
+++ db/schema.rb
@@ -0,0 +1,38 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_04_04_205857) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "url"
+    t.bigint "stream_service_id", null: false
+    t.string "synopsis"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["stream_service_id"], name: "index_movies_on_stream_service_id"
+  end
+
+  create_table "stream_services", force: :cascade do |t|
+    t.string "name"
+    t.string "base_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "code"
+  end
+
+  add_foreign_key "movies", "stream_services"
+end
