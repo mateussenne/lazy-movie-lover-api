5934124ffd9a684accba39c4712547a40974c3cb
Mateus Senne <mateussenne@gmail.com>
2022-04-04 17:58:26 -0300

    Add optional false to movie creation reguarding the StreamService

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Movie < ApplicationRecord
-  belongs_to :stream_service
+  belongs_to :stream_service, optional: false
   validates :name, :url, :image, presence: true
 end
