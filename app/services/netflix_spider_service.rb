7f31ed119bf5a98a5ac7ec046cb9dedf41fb738a
Mateus Senne <mateussenne@gmail.com>
2022-04-04 22:01:16 -0300

    Reident netflix spider

--- app/services/netflix_spider_service.rb
+++ app/services/netflix_spider_service.rb
@@ -20,9 +20,9 @@ class NetflixSpiderService < Kimurai::Base
     scrape_response = browser.current_response
     scrape_response.css('ul.nm-content-horizontal-row-item-container').each do |movies_row|
       movies_row.css('li.nm-content-horizontal-row-item').each do |movie|
-        url = movie.css('a.nm-collections-title nm-collections-link')
+        url =   movie.css('a.nm-collections-title nm-collections-link')
         image = movie.css('img.nm-collections-title-img')
-        name = movie.css('span.nm-collections-title-name')
+        name =  movie.css('span.nm-collections-title-name')
 
         save_movie(name, image, url)
       end
