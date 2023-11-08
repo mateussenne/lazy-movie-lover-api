1f1a97f1a75a62825477486ec20d866374b12e36
Mateus Senne <mateussenne@gmail.com>
2023-11-08 10:57:20 -0300

    Fix case sensitive search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,7 +8,7 @@ class Movie < ApplicationRecord
   validates :slug, presence: true, uniqueness: true
   self.per_page = 30
 
-  scope :search_by_name, ->(params) { where('movies.name LIKE ?', "%#{params}%") }
+  scope :search_by_name, ->(params) { where('LOWER(movies.name) LIKE ?', "%#{params.downcase}%") }
   scope :filter_by_stream_service, lambda { |stream_service|
                                      joins(:stream_service).where(stream_service: { code: stream_service })
                                    }
