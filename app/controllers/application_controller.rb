65483cd6d3a002ec107612ef5d7bb0e36c89a7b3
Mateus Senne <mateussenne@gmail.com>
2023-11-07 22:55:24 -0300

    Remove API controller and pass logic to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,32 @@
 class ApplicationController < ActionController::API
   before_action :authorized?
 
-  def authorization?
-    params[:access_key] == Rails.application.credentials.dig(:navigation, :access_key)
+  def current_user
+    return if decoded_token.blank?
+
+    User.find(decoded_token)
   end
 
+  def encode_token(payload)
+    JWT.encode(payload.to_s, ENV.fetch('authorization_secret'), 'HS256')
+  end
+
+  private
+
   def authorized?
-    render json: { message: 'please contact the administrator' }, status: :unauthorized unless authorization?
+    return head :unauthorized if decoded_token.blank?
+  end
+
+  def decoded_token
+    return if encoded_token.blank? || encoded_token == 'Bearer'
+
+    token = encoded_token.split.last
+    JWT.decode(token, ENV.fetch('authorization_secret'), true, algorithm: 'HS256')[0]
+  rescue
+    nil
+  end
+
+  def encoded_token
+    request.headers['Authorization']
   end
 end
